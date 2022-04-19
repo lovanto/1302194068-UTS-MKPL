2f0707bdf6580e927fb72a1022ada1fe7e1f5895
Lovanto R <rifkylovanto@gmail.com>
2022-04-20 05:30:51 +0700

    Update Employee.java

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -1,48 +1,36 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Employee {
-
+public class Employee extends Human{
 	private String employeeId;
-	private String firstName;
-	private String lastName;
 	private String idNumber;
-	private String address;
-	
+	private Human dataHuman;
 	private int yearJoined;
 	private int monthJoined;
 	private int dayJoined;
 	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
 	private int annualDeductible;
-	
 	private String spouseName;
 	private String spouseIdNumber;
-
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId,   String idNumber,  int yearJoined, int monthJoined, int dayJoined, Human dataHuman) {
 		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
 		this.idNumber = idNumber;
-		this.address = address;
 		this.yearJoined = yearJoined;
 		this.monthJoined = monthJoined;
 		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
+		dataHuman.setFirstName("First Name");
+		dataHuman.setLastName("Last Name");
+		dataHuman.setAddress("Address");
+		dataHuman.setIsForeigner(false);
+		dataHuman.setGender(true);
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
 	}
@@ -51,21 +39,20 @@ public class Employee {
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
-	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
+			if (dataHuman.getIsForeigner()) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		}else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
+			if (dataHuman.getIsForeigner()) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		}else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
+			if (dataHuman.getIsForeigner()) {
 				monthlySalary = (int) (3000000 * 1.5);
 			}
 		}
@@ -88,10 +75,9 @@ public class Employee {
 		childNames.add(childName);
 		childIdNumbers.add(childIdNumber);
 	}
-	
+
+	//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
 		if (date.getYear() == yearJoined) {
