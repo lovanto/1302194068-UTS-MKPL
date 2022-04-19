a0faace40a028c4ada2da13bd1e9ec4b2fb17a86
Lovanto R <rifkylovanto@gmail.com>
2022-04-20 06:00:37 +0700

    delete if curly braces

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -3,29 +3,17 @@ package lib;
 public class TaxFunction {
 
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
 		int tax = 0;
 		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
+		if (numberOfMonthWorking > 12) System.err.println("More than 12 month working per year");
 		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
+		if (numberOfChildren > 3) numberOfChildren = 3;
 		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
+		if (isMarried) tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+		else tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
 		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
+		if (tax < 0) return 0;
+		else return tax;	 
 	}
 	
 }
